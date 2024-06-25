48e4d308ecdf646f13f291c944d04340a5a86989
saira-gulamali <sguacamole98@gmail.com>
2024-06-25 15:27:55 +0100

    basic api functionality of user & jobs - no search functionality

---
+++ src/pages/dashboard/Stats.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { StatsContainer, Loading, ChartsContainer } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
+import { showStats } from "../../features/allJobs/allJobsSlice";
+
+const Stats = () => {
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showStats());
+  }, []);
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications && monthlyApplications.length > 0 && (
+        <ChartsContainer />
+      )}
+    </>
+  );
+};
+export default Stats;
